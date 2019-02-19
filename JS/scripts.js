35b294e78cc00f2aabd762984bab42ec8f1cec73
Lela Smith and Uriel Gonzalez <uriel@>
2019-02-18 20:44:32 -0800

    Add celebrities

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,10 +11,39 @@ $(document).ready(function() {
 
       }
 
-    if (gender === 'female' && typeOf === 'singer' && date === 'beach'){
-        result = 'Lady Gaga';
+    if (gender === 'female' && typeOf === 'movieStar' && date === 'cruise'){
+        result = 'Meryl Streep';
       }
 
+    if (gender === 'female' && typeOf === 'movieStar' && date === 'fancyDinner'){
+          result = 'Penelope Cruz';
+        }
+
+    if (gender === 'female' && typeOf === 'singer' && date === 'beach'){
+          result = 'Beyonce';
+
+          }
+
+    if (gender === 'female' && typeOf === 'singer' && date === 'cruise'){
+            result = 'Lady Gaga';
+          }
+
+    if (gender === 'female' && typeOf === 'singer' && date === 'fancyDinner'){
+              result = 'Barbara Streisand';
+            }
+
+    if (gender === 'female' && typeOf === 'influencer' && date === 'beach'){
+                  result = 'Kim Kardashian';
+
+                  }
+
+    if (gender === 'female' && typeOf === 'influencer' && date === 'cruise'){
+                    result = 'Kylie Jenner';
+                  }
+
+    if (gender === 'female' && typeOf === 'influencer' && date === 'fancyDinner'){
+                      result = 'Lele Pons';
+                    }
     $("#celeb").text(result);
     $("#result").show();
   });
